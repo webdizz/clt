586d75e0b82f558d1c622bb4cd6ada06351e2109
webdizz <webdizz@gmail.com>
2010-03-26 15:45:57 +0200

    Inserting and selecting all translations using database was implemented

--- clt-crx/src/name/webdizz/clt/crx/client/presenter/StorePresenter.java
+++ clt-crx/src/name/webdizz/clt/crx/client/presenter/StorePresenter.java
@@ -3,9 +3,17 @@
  */
 package name.webdizz.clt.crx.client.presenter;
 
+import java.util.List;
+
 import name.webdizz.clt.crx.client.Alert;
+import name.webdizz.clt.crx.client.db.LocalDataService;
+import name.webdizz.clt.crx.client.db.Translation;
 import name.webdizz.clt.crx.client.event.message.StoreTranslationMessage;
 
+import com.google.code.gwt.database.client.service.DataServiceException;
+import com.google.code.gwt.database.client.service.RowIdListCallback;
+import com.google.code.gwt.database.client.service.VoidCallback;
+import com.google.gwt.core.client.GWT;
 import com.mvp4g.client.annotation.Presenter;
 import com.mvp4g.client.event.EventBus;
 import com.mvp4g.client.presenter.BasePresenter;
@@ -24,6 +32,21 @@ public class StorePresenter extends
 	public static class StorePresenterView implements IStorePresenterView {
 	}
 
+	private LocalDataService service = GWT.create(LocalDataService.class);
+
+	public StorePresenter() {
+		service.initDatabase(new VoidCallback() {
+			public void onFailure(DataServiceException error) {
+				// TODO: should have more suitable error reporting approach
+				Alert.info(error.getMessage());
+			}
+
+			public void onSuccess() {
+				// currently do nothing
+			}
+		});
+	}
+
 	/**
 	 * Is invoked on storing translation.
 	 * 
@@ -32,5 +55,23 @@ public class StorePresenter extends
 	 */
 	public void onStoreTranslation(StoreTranslationMessage message) {
 		Alert.info(message.getTranslateable());
+		Translation translation = Translation.instance();
+		translation.setText(message.getTranslateable());
+		translation.setTranslation(message.getTranslation());
+		// TODO: those attributes should be resolved from app
+		translation.setDest("ua");
+		translation.setSrc("en");
+		Alert.info("te be stored");
+		service.storeTranslation(translation, new RowIdListCallback() {
+
+			public void onFailure(DataServiceException error) {
+				// TODO: should have more suitable error reporting approach
+				Alert.info(error.getMessage());
+			}
+
+			public void onSuccess(List<Integer> rowIds) {
+				Alert.info("Translation was stored. " + rowIds.size());
+			}
+		});
 	}
 }
