8aa32cbd605575fbfcf62dbdec6476b5840330cd
webdizz <webdizz@gmail.com>
2010-07-10 00:36:03 +0300

    Validation methods were replaced by Validator ones

--- gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/linker/emiter/Emiter.java
+++ gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/linker/emiter/Emiter.java
@@ -17,7 +17,23 @@ import com.google.gwt.core.ext.typeinfo.JClassType;
 public interface Emiter {
 
 	static final String ICON_USER_TYPE = "com.google.gwt.chrome.crx.client.Icon";
-	
+
+	/**
+	 * Abstract method to be implemented by {@link Emiter}s to support code <br/>
+	 * emision of Chrome extension artifacts e.g. BrowserAction, PageAction etc.
+	 * 
+	 * @param logger
+	 *            the {@link TreeLogger}
+	 * @param context
+	 *            the {@link GeneratorContext}
+	 * @param userType
+	 *            the {@link JClassType}
+	 * @param typeName
+	 *            the emision artifact class name
+	 * @return emited class name
+	 * @throws UnableToCompleteException
+	 *             if emison cannot be completed
+	 */
 	String emit(TreeLogger logger, GeneratorContext context, JClassType userType, String typeName)
 			throws UnableToCompleteException;
 
