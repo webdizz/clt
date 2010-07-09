f427d8f5119bcd0de3174f85e2303ae7dfe98ef0
webdizz <webdizz@gmail.com>
2010-07-09 19:23:00 +0300

    Write some tests for BrowserActionEmiter

--- gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/linker/emiter/Emiter.java
+++ gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/linker/emiter/Emiter.java
@@ -3,19 +3,20 @@
  */
 package com.google.gwt.chrome.crx.linker.emiter;
 
-import com.google.gwt.chrome.crx.linker.emision.Emision;
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
 
 /**
  * @author webdizz
- * @param <E> the type of emision to be emited
+ * @param <E>
+ *            the type of emision to be emited
  * 
  */
-public interface Emiter<E extends Emision> {
+public interface Emiter {
 
-	String emit(TreeLogger logger, GeneratorContext context, JClassType userType,
-			String typeName);
+	String emit(TreeLogger logger, GeneratorContext context, JClassType userType, String typeName)
+			throws UnableToCompleteException;
 
 }
