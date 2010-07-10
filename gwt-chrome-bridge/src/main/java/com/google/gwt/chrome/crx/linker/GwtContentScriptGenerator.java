7b2ba8ada94d7d7460347ca1ed26bb274d7904b5
webdizz <webdizz@gmail.com>
2010-07-11 01:45:58 +0300

    Implementation and tests for GwtContentScriptGenerator were added

---
+++ gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/linker/GwtContentScriptGenerator.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.google.gwt.chrome.crx.linker;
+
+import com.google.gwt.chrome.crx.client.GwtContentScript;
+import com.google.gwt.chrome.crx.client.GwtContentScript.ManifestInfo;
+import com.google.gwt.chrome.crx.linker.artifact.GwtContentScriptArtifact;
+import com.google.gwt.chrome.crx.linker.emiter.Validator;
+import com.google.gwt.core.ext.Generator;
+import com.google.gwt.core.ext.GeneratorContext;
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.NotFoundException;
+import com.google.gwt.user.rebind.ClassSourceFileComposerFactory;
+
+/**
+ * @author webdizz
+ * 
+ */
+public class GwtContentScriptGenerator extends Generator {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.google.gwt.core.ext.Generator#generate(com.google.gwt.core.ext.TreeLogger
+	 * , com.google.gwt.core.ext.GeneratorContext, java.lang.String)
+	 */
+	@Override
+	public String generate(final TreeLogger logger, final GeneratorContext context, final String typeName)
+			throws UnableToCompleteException {
+		Validator<ManifestInfo> validator;
+		validator = new Validator<ManifestInfo>(logger, GwtContentScript.class.getSimpleName(), typeName);
+		try {
+			JClassType userType;
+			userType = context.getTypeOracle().getType(typeName);
+			final ManifestInfo spec = userType.getAnnotation(ManifestInfo.class);
+			validator.ensureAnnotatedWithManifest(spec);
+
+			// create artifact
+			String path = userType.getSimpleSourceName();
+			GwtContentScriptArtifact artifact;
+			artifact = new GwtContentScriptArtifact(path, spec.matches(), spec.runAt(), spec.allFrames());
+			context.commitArtifact(logger, artifact);
+
+			// generate extension type
+			final String subclassName = userType.getSimpleSourceName().replace('.', '_') + "_generated";
+			final String packageName = userType.getPackage().getName();
+			final ClassSourceFileComposerFactory composerFactory = new ClassSourceFileComposerFactory(packageName,
+					subclassName);
+			composerFactory.setSuperclass(userType.getQualifiedSourceName());
+			context.tryCreate(logger, packageName, subclassName);
+			// TODO: implement class if needed
+			return composerFactory.getCreatedClassName();
+		} catch (NotFoundException e) {
+			logger.log(TreeLogger.ERROR, "Unsupported Type: " + typeName);
+			throw new UnableToCompleteException();
+		}
+	}
+
+}
---
+++ gwt-chrome-bridge/src/test/java/com/google/gwt/chrome/crx/linker/emiter/GwtContentScriptGeneratorTest.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ */
+package com.google.gwt.chrome.crx.linker.emiter;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.google.gwt.chrome.crx.client.GwtContentScript;
+import com.google.gwt.chrome.crx.linker.GwtContentScriptGenerator;
+import com.google.gwt.chrome.crx.linker.artifact.GwtContentScriptArtifact;
+import com.google.gwt.core.ext.Generator;
+import com.google.gwt.core.ext.GeneratorContext;
+import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.JPackage;
+import com.google.gwt.core.ext.typeinfo.NotFoundException;
+import com.google.gwt.core.ext.typeinfo.TypeOracle;
+
+/**
+ * @author webdizz
+ * 
+ */
+public class GwtContentScriptGeneratorTest {
+
+	@Mock
+	private TreeLogger logger;
+
+	@Mock
+	private GeneratorContext context;
+
+	@Mock
+	private JClassType userType;
+
+	@Mock
+	private GwtContentScript.ManifestInfo spec;
+
+	private static final String TYPE_NAME = Emiter.GWT_CONTENTSCRIPT_USER_TYPE;
+
+	private static final String PATH = TYPE_NAME.replace('.', '_') + "_generated";
+
+	private static final String PACKAGE_NAME = TYPE_NAME.substring(0, TYPE_NAME.lastIndexOf('.'));
+
+	@Before
+	public void setUp() throws NotFoundException {
+		MockitoAnnotations.initMocks(this);
+
+		TypeOracle typeOracle = mock(TypeOracle.class);
+		when(userType.getAnnotation(GwtContentScript.ManifestInfo.class)).thenReturn(spec);
+		when(userType.getSimpleSourceName()).thenReturn(PATH);
+		JPackage jPackage = mock(JPackage.class);
+		when(jPackage.getName()).thenReturn(PACKAGE_NAME);
+		when(userType.getPackage()).thenReturn(jPackage);
+		when(typeOracle.getType(TYPE_NAME)).thenReturn(userType);
+		when(context.getTypeOracle()).thenReturn(typeOracle);
+	}
+
+	@Test(expected = UnableToCompleteException.class)
+	public void shouldThrowUnableToCompleteExceptionIfUnsupportedType() throws UnableToCompleteException,
+			NotFoundException {
+
+		TypeOracle typeOracle = mock(TypeOracle.class);
+		when(typeOracle.getType(TYPE_NAME)).thenThrow(new NotFoundException());
+		when(context.getTypeOracle()).thenReturn(typeOracle);
+
+		invokeCodeGeneration();
+	}
+
+	@Test(expected = UnableToCompleteException.class)
+	public void shouldThrowUnableToCompleteExceptionIfNoManifestAnnotation() throws UnableToCompleteException {
+		when(userType.getAnnotation(GwtContentScript.ManifestInfo.class)).thenReturn(null);
+
+		invokeCodeGeneration();
+	}
+
+	@Test
+	public void shouldCreateArtifactFromSpec() throws UnableToCompleteException {
+		invokeCodeGeneration();
+
+		verify(spec).allFrames();
+		verify(spec).runAt();
+	}
+
+	@Test
+	public void shouldCommitCreatedArtifact() throws UnableToCompleteException {
+		GwtContentScriptArtifact artifact;
+		artifact = new GwtContentScriptArtifact(PATH, spec.matches(), spec.runAt(), spec.allFrames());
+
+		invokeCodeGeneration();
+
+		verify(context).commitArtifact(logger, artifact);
+	}
+
+	@Test
+	public void shouldGenerateNameForCreatingArtifactType() throws UnableToCompleteException {
+
+		invokeCodeGeneration();
+
+		verify(userType, times(2)).getSimpleSourceName();
+	}
+
+	@Test
+	public void shouldResolvePackageName() throws UnableToCompleteException {
+		invokeCodeGeneration();
+
+		verify(userType).getPackage();
+	}
+
+	@Test
+	public void shouldObtainQualifiedSourceName() throws UnableToCompleteException {
+		invokeCodeGeneration();
+
+		verify(userType).getQualifiedSourceName();
+	}
+
+	@Test
+	public void shouldCalltryCreateOnContext() throws UnableToCompleteException {
+		invokeCodeGeneration();
+
+		verify(context).tryCreate(logger, PACKAGE_NAME, PATH + "_generated");
+	}
+
+	@Test
+	public void shouldReturnCreatedClassName() throws UnableToCompleteException {
+		String createdClass = invokeCodeGeneration();
+
+		assertEquals(PACKAGE_NAME + '.' + PATH + "_generated", createdClass);
+	}
+
+	protected String invokeCodeGeneration() throws UnableToCompleteException {
+		Generator generator = new GwtContentScriptGenerator();
+		return generator.generate(logger, context, TYPE_NAME);
+	}
+}
