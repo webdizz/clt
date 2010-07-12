96c7c4d03238f198d2efb717eab9a2f76bb9928f
webdizz <webdizz@gmail.com>
2010-07-12 13:01:29 +0300

    GwtContentScript class shold be a component and linked as a component into extension

--- gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/client/GwtContentScript.java
+++ gwt-chrome-bridge/src/main/java/com/google/gwt/chrome/crx/client/GwtContentScript.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2009 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.google.gwt.chrome.crx.client;
 
 import java.lang.annotation.Documented;
@@ -21,8 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.google.gwt.core.client.EntryPoint;
-
 /**
  * 
  * {@link GwtContentScript} is a {@link ContentScript} the main difference is it
@@ -30,7 +13,7 @@ import com.google.gwt.core.client.EntryPoint;
  * href="http://code.google.com/chrome/extensions/content_scripts.html" >Content
  * Scripts</a>
  */
-public abstract class GwtContentScript implements EntryPoint {
+public abstract class GwtContentScript implements Component {
 
 	/**
 	 * In the case of "document_start", the files are injected after any files
@@ -102,15 +85,4 @@ public abstract class GwtContentScript implements EntryPoint {
 		boolean allFrames() default false;
 	}
 
-	/**
-	 * Should be implemented in you {@link GwtContentScript}s.<br />
-	 * This is a start point for {@link GwtContentScript} code.
-	 */
-	abstract public void onScriptLoad();
-
-	public void onModuleLoad() {
-		onScriptLoad();
-	}
-
-	// TODO: add abilities to connect to other processes from script
 }
