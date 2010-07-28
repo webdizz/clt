a81a25e0dad7ebb3bc2d5c5d166023539008a800
webdizz <webdizz@gmail.com>
2010-07-29 00:09:57 +0300

    hashCode and equals methods implementation was added

--- clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/TranslationResult.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/TranslationResult.java
@@ -74,4 +74,57 @@ public class TranslationResult {
 		this.destLang = destLang;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dest == null) ? 0 : dest.hashCode());
+		result = prime * result + ((destLang == null) ? 0 : destLang.hashCode());
+		result = prime * result + ((src == null) ? 0 : src.hashCode());
+		result = prime * result + ((srcLang == null) ? 0 : srcLang.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TranslationResult other = (TranslationResult) obj;
+		if (dest == null) {
+			if (other.dest != null)
+				return false;
+		} else if (!dest.equals(other.dest))
+			return false;
+		if (destLang == null) {
+			if (other.destLang != null)
+				return false;
+		} else if (!destLang.equals(other.destLang))
+			return false;
+		if (src == null) {
+			if (other.src != null)
+				return false;
+		} else if (!src.equals(other.src))
+			return false;
+		if (srcLang == null) {
+			if (other.srcLang != null)
+				return false;
+		} else if (!srcLang.equals(other.srcLang))
+			return false;
+		return true;
+	}
+
 }
