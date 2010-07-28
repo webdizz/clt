640d9e0c3e390a4f53861dcf2b96a9c54ec217f7
webdizz <webdizz@gmail.com>
2010-07-28 19:08:21 +0300

    Method comment was defined

--- clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/Translator.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/Translator.java
@@ -3,13 +3,23 @@
  */
 package name.webdizz.clt.crx.client.translation;
 
-
 /**
  * @author webdizz
  * 
  */
 public interface Translator {
 
+	/**
+	 * Performs translation of the given word and passing result to
+	 * {@link TranslationHandler}.
+	 * 
+	 * @param word
+	 *            the word to translate
+	 * @param handler
+	 *            the {@link TranslationHandler} to pass result to
+	 * @throws TranslationException
+	 *             translations did not success
+	 */
 	void translate(String word, TranslationHandler handler) throws TranslationException;
 
 }
