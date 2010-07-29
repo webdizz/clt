99fa92d746b0b8018f531f89bbea3d497dae8129
webdizz <webdizz@gmail.com>
2010-07-29 15:53:14 +0300

    Language class was added to hold language keys

---
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/Language.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package name.webdizz.clt.crx.client.translation;
+
+/**
+ * @author webdizz
+ * 
+ */
+public class Language {
+
+	public static final String RUSSIAN = com.google.gwt.language.client.translation.Language.RUSSIAN.getLangCode();
+	public static final String CRIMEAN_TATAR = "crh";
+}
