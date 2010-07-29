741fe02ed1df6b634ccb9e6b97e35f05da87da54
webdizz <webdizz@gmail.com>
2010-07-30 00:56:57 +0300

    TranslationResultCreator refactoring and enhancements

--- clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/medeniye/TranslationResultCreator.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/medeniye/TranslationResultCreator.java
@@ -19,6 +19,8 @@ import name.webdizz.clt.crx.client.translation.TranslationResult.Translation;
  */
 public class TranslationResultCreator {
 
+	private static final int ZERO = 0;
+
 	private static final int NOTHING_FOUND = -1;
 
 	private static final String RUSSIAN_WORD = "Russian";
@@ -61,75 +63,74 @@ public class TranslationResultCreator {
 		String transTxt = isParsable ? text.substring(transStart + TRANSLATIONS_START_SMB.length(), transEnd) : "";
 		List<Translation> translations = new ArrayList<Translation>();
 		if (!transTxt.isEmpty()) {
-			boolean work = true;
-			while (work) {
-				int pos = transTxt.indexOf(TRANSLATION_START_SMB);
-				work = pos > NOTHING_FOUND;
-				if (work) {
-					Translation translation = translationResult.new Translation();
-					int start = pos + TRANSLATION_START_SMB.length();
-					int end = transTxt.substring(start).indexOf(DIV_END_SMB) + start;
-					String translationText = transTxt.substring(start, end);
-					translation.setTranslation(translationText);
-					translations.add(translation);
-					// handle explanations
-					transTxt = processExplanations(translationResult, transTxt, translation, end);
-				}
-			}
+			processTranslations(translationResult, transTxt, translations);
 		}
 		if (!translations.isEmpty()) {
 			translationResult.setTranslations(translations);
 		}
 	}
 
-	private String processExplanations(final TranslationResult translationResult, String translationTxt,
-			Translation translation, int end) {
+	private void processTranslations(final TranslationResult translationResult, final String translationTxt,
+			final List<Translation> translations) {
+		String transTxt = translationTxt;
+		int pos = transTxt.indexOf(TRANSLATION_START_SMB);
+		while (pos > NOTHING_FOUND) {
+			int start = pos + TRANSLATION_START_SMB.length();
+			int end = transTxt.substring(start).indexOf(DIV_END_SMB) + start;
+			Translation translation = createAndAddTranslation(translationResult, transTxt, translations, start, end);
+			// handle explanations
+			transTxt = processExplanations(translationResult, transTxt, translation, end);
+			pos = transTxt.indexOf(TRANSLATION_START_SMB);
+		}
+	}
+
+	private Translation createAndAddTranslation(final TranslationResult translationResult, String transTxt,
+			List<Translation> translations, int start, int end) {
+		String translationText = transTxt.substring(start, end);
+		Translation translation = translationResult.new Translation();
+		translation.setTranslation(translationText);
+		translations.add(translation);
+		return translation;
+	}
+
+	private String processExplanations(final TranslationResult translationResult, final String translationTxt,
+			final Translation translation, final int endPosition) {
 		String transTxt = new String(translationTxt);
+		int end = endPosition;
 		int explStart = transTxt.indexOf(EXPLAINATION_START_SMB);
 		int explSmbLength = EXPLAINATION_START_SMB.length();
+		List<Explanation> explanations = new ArrayList<TranslationResult.Explanation>();
 		if (explStart > NOTHING_FOUND) {
-			List<Explanation> explanations = new ArrayList<TranslationResult.Explanation>();
 			boolean workWithExplanations = true;
-
 			while (workWithExplanations) {
 				explStart = explStart + explSmbLength;
 				transTxt = transTxt.substring(explStart);
-				String explanationTxt = transTxt.substring(0, transTxt.indexOf(DIV_END_SMB));
+				String explanationTxt = transTxt.substring(ZERO, transTxt.indexOf(DIV_END_SMB));
 				createAndAddExplanation(translationResult, explanations, explanationTxt);
-				workWithExplanations = isContinueable(transTxt, explanationTxt);
+				workWithExplanations = nextExplanationExists(transTxt, explanationTxt);
 
+				// reset start point
 				transTxt = transTxt.substring(explanationTxt.length() + DIV_END_SMB.length());
-				explStart = 0;// reset start point
-				end = 0;
-			}
-			if (!explanations.isEmpty()) {
-				translation.setExplanations(explanations);
+				explStart = ZERO;
+				end = ZERO;
 			}
 		}
-
+		if (!explanations.isEmpty()) {
+			translation.setExplanations(explanations);
+		}
 		// prepare data for next iteration
-		transTxt = transTxt.substring(end);
-		return transTxt;
+		return transTxt.substring(end);
 	}
 
-	/**
-	 * @param transTxt
-	 * @param explanationTxt
-	 * @return
-	 */
-	private boolean isContinueable(final String transTxt, final String explanationTxt) {
+	private boolean nextExplanationExists(final String transTxt, final String explanationTxt) {
 		boolean workWithExplanations;
 		int explSmbLength = EXPLAINATION_START_SMB.length();
 		int subStart = explanationTxt.length() + DIV_END_SMB.length();
-		workWithExplanations = EXPLAINATION_START_SMB.equals(transTxt.substring(subStart, subStart + explSmbLength));
+		int subEnd = subStart + explSmbLength;
+		workWithExplanations = EXPLAINATION_START_SMB.equals(transTxt.substring(subStart, subEnd));
 		return workWithExplanations;
 	}
 
-	/**
-	 * @param translationResult
-	 * @param explanations
-	 * @param explanationTxt
-	 */
 	private void createAndAddExplanation(final TranslationResult translationResult,
 			final List<Explanation> explanations, final String explanationTxt) {
 		Explanation explanation = translationResult.new Explanation();
