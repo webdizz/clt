a3d6b0b30c90a580fb2a729bb56d7c1b13229935
webdizz <webdizz@gmail.com>
2010-08-01 12:20:18 +0300

    Sending message to background page was implemented

--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
@@ -3,15 +3,32 @@
  */
 package name.webdizz.clt.crx.contentscript.client;
 
+import name.webdizz.clt.crx.client.ActivationKeysHolder;
+import name.webdizz.clt.crx.client.event.message.SelectTextMessage;
+
 import com.google.gwt.chrome.crx.client.GwtContentScriptEntryPoint;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.chrome.crx.client.Port;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.Event.NativePreviewHandler;
 
 /**
  * @author webdizz
  * 
  */
 public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
+
+	private static class SelectionProvider extends JavaScriptObject {
+		protected SelectionProvider() {
+		}
+
+		final static native String getSelection() /*-{
+			var selection = $wnd.getSelection();
+			return selection?new String(selection.toString()).trim():'';
+		}-*/;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -22,10 +39,26 @@ public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
 	 */
 	@Override
 	public void onScriptLoad() {
-		RootPanel panel = RootPanel.get();
-		if (null != panel) {
-			Window.alert(panel.toString());
-		}
+		Event.addNativePreviewHandler(new NativePreviewHandler() {
+
+			public void onPreviewNativeEvent(final NativePreviewEvent preview) {
+				NativeEvent event = preview.getNativeEvent();
+				String selection = SelectionProvider.getSelection();
+				if (isConsumed(event, selection)) {
+					// Send event to background page
+					Port port = connect("liner");
+					port.postMessage(SelectTextMessage.create(selection, event));
+					// Tell the event handler that this event has been consumed
+					preview.consume();
+				}
+			}
+
+			private boolean isConsumed(NativeEvent event, String selection) {
+				return "mouseup".equals(event.getType()) && ActivationKeysHolder.isKeysAllowed(event)
+						&& !selection.isEmpty();
+			}
+		});
+
 	}
 
 }
