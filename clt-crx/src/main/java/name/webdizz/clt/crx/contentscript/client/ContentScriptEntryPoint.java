f8786b96b46edf5d2178dc405ee65dc2fc59ea5b
webdizz <webdizz@gmail.com>
2010-08-02 20:48:37 +0300

    Sample implementation of the content script requests from extension listener was added

--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
@@ -5,14 +5,20 @@ package name.webdizz.clt.crx.contentscript.client;
 
 import name.webdizz.clt.crx.client.chrome.ChromePort;
 
+import com.google.gwt.chrome.crx.client.Chrome;
 import com.google.gwt.chrome.crx.client.GwtContentScriptEntryPoint;
+import com.google.gwt.chrome.crx.client.events.Message;
+import com.google.gwt.chrome.crx.client.events.RequestEvent.Listener;
 import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
 
 /**
  * @author webdizz
  * 
  */
 public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
+
+	private static final String CONNECTION_NAME = "liner";
 
 	/*
 	 * (non-Javadoc)
@@ -23,8 +29,18 @@ public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
 	 */
 	@Override
 	public void onScriptLoad() {
-		ChromePort port = new ChromePort(connect("liner"));
+		ChromePort port = new ChromePort(connect(CONNECTION_NAME));
+		// assign native event handler
 		Event.addNativePreviewHandler(new ContentScriptEventHandler(port));
-	}
 
+		Chrome.getExtension().getOnRequestEvent().addListener(new Listener() {
+			public void onRequest(Message message) {
+				if (null == message) {
+					Window.alert("message is null");
+				}
+				Window.alert("Wow");
+			}
+		});
+
+	}
 }
