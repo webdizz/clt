bcc93e6145463775bbda217eec5ff04a8b9d3c9a
webdizz <webdizz@gmail.com>
2010-08-02 11:46:11 +0300

    Refactoring was performed: NativePreviewHandler and SelectionProvider were extracted into separate classes

--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/ContentScriptEntryPoint.java
@@ -3,32 +3,16 @@
  */
 package name.webdizz.clt.crx.contentscript.client;
 
-import name.webdizz.clt.crx.client.ActivationKeysHolder;
-import name.webdizz.clt.crx.client.event.message.SelectTextMessage;
+import name.webdizz.clt.crx.client.chrome.ChromePort;
 
 import com.google.gwt.chrome.crx.client.GwtContentScriptEntryPoint;
-import com.google.gwt.chrome.crx.client.Port;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.Event.NativePreviewEvent;
-import com.google.gwt.user.client.Event.NativePreviewHandler;
 
 /**
  * @author webdizz
  * 
  */
 public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
-
-	private static class SelectionProvider extends JavaScriptObject {
-		protected SelectionProvider() {
-		}
-
-		final static native String getSelection() /*-{
-			var selection = $wnd.getSelection();
-			return selection?new String(selection.toString()).trim():'';
-		}-*/;
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -39,26 +23,8 @@ public class ContentScriptEntryPoint extends GwtContentScriptEntryPoint {
 	 */
 	@Override
 	public void onScriptLoad() {
-		Event.addNativePreviewHandler(new NativePreviewHandler() {
-
-			public void onPreviewNativeEvent(final NativePreviewEvent preview) {
-				NativeEvent event = preview.getNativeEvent();
-				String selection = SelectionProvider.getSelection();
-				if (isConsumed(event, selection)) {
-					// Send event to background page
-					Port port = connect("liner");
-					port.postMessage(SelectTextMessage.create(selection, event));
-					// Tell the event handler that this event has been consumed
-					preview.consume();
-				}
-			}
-
-			private boolean isConsumed(NativeEvent event, String selection) {
-				return "mouseup".equals(event.getType()) && ActivationKeysHolder.isKeysAllowed(event)
-						&& !selection.isEmpty();
-			}
-		});
-
+		ChromePort port = new ChromePort(connect("liner"));
+		Event.addNativePreviewHandler(new ContentScriptEventHandler(port, new SelectionProvider()));
 	}
 
 }
