0ab12c58514ee85ffea8d98a597f879b082b7781
webdizz <webdizz@gmail.com>
2010-08-03 11:59:01 +0300

    Now translation result can be displayed from content script using GWT

--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/connection/ContentScriptRequestListener.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/connection/ContentScriptRequestListener.java
@@ -1,8 +1,12 @@
 package name.webdizz.clt.crx.contentscript.client.connection;
 
+import name.webdizz.clt.crx.client.event.message.ShowTranslatedTextMessage;
+
 import com.google.gwt.chrome.crx.client.events.Message;
 import com.google.gwt.chrome.crx.client.events.RequestEvent.Listener;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.PopupPanel;
 
 /**
  * Listener for request communications from extension or another content script.
@@ -12,6 +16,18 @@ import com.google.gwt.user.client.Window;
  */
 public final class ContentScriptRequestListener implements Listener {
 
+	private static class TranslationResultPanel extends PopupPanel {
+		public TranslationResultPanel(ShowTranslatedTextMessage message) {
+			// enable autohide
+			super(true);
+			setTitle("Title");
+			setAnimationEnabled(true);
+			setGlassEnabled(true);
+			HTML widget = new HTML(message.getWidget());
+			setWidget(widget);
+		}
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -20,9 +36,19 @@ public final class ContentScriptRequestListener implements Listener {
 	 * (com.google.gwt.chrome.crx.client.events.Message)
 	 */
 	public void onRequest(final Message message) {
-		if (null == message) {
-			Window.alert("message is null");
+		if (null != message) {
+			final name.webdizz.clt.crx.client.event.message.Message castedMessage = message.cast();
+			if (ShowTranslatedTextMessage.TYPE.equals(castedMessage.getType())) {
+				final ShowTranslatedTextMessage translation = castedMessage.cast();
+				final TranslationResultPanel resultPanel = new TranslationResultPanel(translation);
+				resultPanel.setPopupPositionAndShow(new PopupPanel.PositionCallback() {
+					public void setPosition(int offsetWidth, int offsetHeight) {
+						int left = (Window.getClientWidth() - offsetWidth) / 3;
+						int top = (Window.getClientHeight() - offsetHeight) / 3;
+						resultPanel.setPopupPosition(left, top);
+					}
+				});
+			}
 		}
-		Window.alert("Wow");
 	}
 }
