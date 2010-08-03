6fb7f68dd1f6d2d5edd30e494fa268a7c0e16047
webdizz <webdizz@gmail.com>
2010-08-03 13:22:59 +0300

    Google translation logic was moved out into separate class GoogleTranslator

--- clt-crx/src/main/java/name/webdizz/clt/crx/client/presenter/TranslationPresenter.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/presenter/TranslationPresenter.java
@@ -3,19 +3,13 @@
  */
 package name.webdizz.clt.crx.client.presenter;
 
-import name.webdizz.clt.crx.client.ExtConfiguration;
 import name.webdizz.clt.crx.client.ExtEventBus;
 import name.webdizz.clt.crx.client.event.message.PrepareTranslatedTextDisplayMessage;
 import name.webdizz.clt.crx.client.event.message.ShowTranslatedTextMessage;
 import name.webdizz.clt.crx.client.event.message.TranslateTextMessage;
+import name.webdizz.clt.crx.client.translation.google.GoogleTranslator;
 import name.webdizz.clt.crx.client.view.TranslationView;
 
-import com.google.gwt.language.client.translation.LangDetCallback;
-import com.google.gwt.language.client.translation.LangDetResult;
-import com.google.gwt.language.client.translation.Language;
-import com.google.gwt.language.client.translation.Translation;
-import com.google.gwt.language.client.translation.TranslationCallback;
-import com.google.gwt.language.client.translation.TranslationResult;
 import com.google.gwt.user.client.ui.Widget;
 import com.mvp4g.client.annotation.Presenter;
 import com.mvp4g.client.presenter.BasePresenter;
@@ -25,25 +19,21 @@ import com.mvp4g.client.presenter.BasePresenter;
  * 
  */
 @Presenter(view = TranslationView.class)
-public class TranslationPresenter extends
-		BasePresenter<TranslationPresenter.ITranslationView, ExtEventBus> {
+public class TranslationPresenter extends BasePresenter<TranslationPresenter.ITranslationView, ExtEventBus> {
 
 	public interface ITranslationView {
-		
+
 		void setTranslatedText(String text);
-		
+
 		void setTranslateableText(final String text);
 
 		String widgetAsString();
 
 		Widget asWidget();
-		
+
 	}
 
-	private ExtConfiguration configuration;
-
 	public TranslationPresenter() {
-		configuration = new ExtConfiguration();
 	}
 
 	/**
@@ -53,7 +43,7 @@ public class TranslationPresenter extends
 	 *            the message to translate
 	 */
 	public void onTranslateText(final TranslateTextMessage message) {
-		detectSrcLanguage(message);
+		new GoogleTranslator(eventBus).translate(message);
 	}
 
 	/**
@@ -63,57 +53,10 @@ public class TranslationPresenter extends
 	 * @param message
 	 *            the translated message to display
 	 */
-	public void onTranslatedText(
-			final PrepareTranslatedTextDisplayMessage message) {
+	public void onTranslatedText(final PrepareTranslatedTextDisplayMessage message) {
 		view.setTranslatedText(message.getTranslation());
 		view.setTranslateableText(message.getTextFrom());
 		eventBus.showTranslatedText(view.asWidget());
 	}
 
-	private void detectSrcLanguage(final TranslateTextMessage message) {
-		Translation.detect(message.getText(), new LangDetCallback() {
-			protected void onCallback(LangDetResult result) {
-				translateText(message, result);
-			}
-		});
-	}
-
-	private void translateText(final TranslateTextMessage message,
-			LangDetResult result) {
-		if (null == result.getError()) {
-			final String src = result.getLanguage();
-			final String dest = resolveDestLanguage();
-			final String from = message.getText();
-			Translation.translate(from, src, dest, new TranslationCallback() {
-				protected void onCallback(TranslationResult result) {
-					handleTranslationResult(src, dest, from, result);
-				}
-			});
-		}
-		if (null != result.getError()) {
-			// TODO: display error message
-		}
-	}
-
-	private void handleTranslationResult(final String src, final String dest,
-			final String from, TranslationResult result) {
-		String translation = result.getTranslatedText();
-		PrepareTranslatedTextDisplayMessage transTextMsg;
-		transTextMsg = PrepareTranslatedTextDisplayMessage.create(from,
-				translation, src, dest);
-		eventBus.handleTranslatedText(transTextMsg);
-	}
-
-	/**
-	 * Resolves destination language from the Browser or user's preferences.
-	 * 
-	 * @return language code
-	 */
-	private String resolveDestLanguage() {
-		String lang = configuration.getDestLanguage();
-		if (null == lang || "".equals(lang)) {
-			lang = Language.RUSSIAN.getLangCode();
-		}
-		return lang;
-	}
 }
---
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/translation/google/GoogleTranslator.java
@@ -0,0 +1,70 @@
+package name.webdizz.clt.crx.client.translation.google;
+
+import name.webdizz.clt.crx.client.ExtConfiguration;
+import name.webdizz.clt.crx.client.ExtEventBus;
+import name.webdizz.clt.crx.client.event.message.PrepareTranslatedTextDisplayMessage;
+import name.webdizz.clt.crx.client.event.message.TranslateTextMessage;
+
+import com.google.gwt.language.client.translation.LangDetCallback;
+import com.google.gwt.language.client.translation.LangDetResult;
+import com.google.gwt.language.client.translation.Language;
+import com.google.gwt.language.client.translation.Translation;
+import com.google.gwt.language.client.translation.TranslationCallback;
+import com.google.gwt.language.client.translation.TranslationResult;
+
+public final class GoogleTranslator {
+
+	private ExtConfiguration configuration;
+
+	private ExtEventBus eventBus;
+
+	public GoogleTranslator(ExtEventBus eventBus) {
+		this.eventBus = eventBus;
+		configuration = new ExtConfiguration();
+	}
+
+	public void translate(final TranslateTextMessage message) {
+		Translation.detect(message.getText(), new LangDetCallback() {
+			protected void onCallback(LangDetResult result) {
+				translateText(message, result);
+			}
+		});
+	}
+
+	private void translateText(final TranslateTextMessage message, LangDetResult result) {
+		if (null == result.getError()) {
+			final String src = result.getLanguage();
+			final String dest = resolveDestLanguage();
+			final String from = message.getText();
+			Translation.translate(from, src, dest, new TranslationCallback() {
+				protected void onCallback(TranslationResult result) {
+					handleTranslationResult(src, dest, from, result);
+				}
+			});
+		}
+		if (null != result.getError()) {
+			// TODO: display error message
+		}
+	}
+
+	/**
+	 * Resolves destination language from the Browser or user's preferences.
+	 * 
+	 * @return language code
+	 */
+	private String resolveDestLanguage() {
+		String lang = configuration.getDestLanguage();
+		if (null == lang || "".equals(lang)) {
+			lang = Language.RUSSIAN.getLangCode();
+		}
+		return lang;
+	}
+
+	private void handleTranslationResult(final String src, final String dest, final String from,
+			TranslationResult result) {
+		String translation = result.getTranslatedText();
+		PrepareTranslatedTextDisplayMessage transTextMsg;
+		transTextMsg = PrepareTranslatedTextDisplayMessage.create(from, translation, src, dest);
+		eventBus.handleTranslatedText(transTextMsg);
+	}
+}
