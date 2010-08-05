ec131f97d44274ac71557fcae540ec4b77e3f072
webdizz <webdizz@gmail.com>
2010-08-05 11:55:59 +0300

    ContentScriptEventHandlerTest was added into tests suite

--- clt-crx/src/test/java/name/webdizz/clt/crx/client/AllTests.java
+++ clt-crx/src/test/java/name/webdizz/clt/crx/client/AllTests.java
@@ -6,6 +6,7 @@ package name.webdizz.clt.crx.client;
 import name.webdizz.clt.crx.client.translation.medeniye.MedeniyeRequestBuilderTest;
 import name.webdizz.clt.crx.client.translation.medeniye.MedeniyeTranslatorTest;
 import name.webdizz.clt.crx.client.translation.medeniye.TranslationResultCreatorTest;
+import name.webdizz.clt.crx.contentscript.client.connection.ContentScriptEventHandlerTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -19,7 +20,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses(value = { 
 	MedeniyeRequestBuilderTest.class, 
 	MedeniyeTranslatorTest.class, 
-	TranslationResultCreatorTest.class 
+	TranslationResultCreatorTest.class,
+	ContentScriptEventHandlerTest.class
 })
 public class AllTests {
 
