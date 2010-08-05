7839950dd8e1b986b6360783d16052f9ed4ce626
webdizz <webdizz@gmail.com>
2010-08-05 11:47:20 +0300

    TranslationResultMessage was refactored to support holding TranslationResult instead of simple translation string

--- clt-crx/src/main/java/name/webdizz/clt/crx/client/event/message/TranslationResultMessage.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/client/event/message/TranslationResultMessage.java
@@ -3,36 +3,25 @@
  */
 package name.webdizz.clt.crx.client.event.message;
 
+import name.webdizz.clt.crx.client.translation.TranslationResult;
+
 /**
  * @author Izzet_Mustafayev
  * 
  */
-public class PrepareTranslatedTextDisplayMessage extends Message {
+public class TranslationResultMessage extends Message {
 
-	protected PrepareTranslatedTextDisplayMessage() {
+	protected TranslationResultMessage() {
 	}
 
-	public static final String TYPE = "PrepareTranslatedTextDisplayMessage";
+	public static final String TYPE = "TranslationResultMessage";
 
-	public final native String getDestLang() /*-{
-		return this.dest;
-	}-*/;
-
-	public final native String getSrcLang() /*-{
-		return this.src;
-	}-*/;
-
-	public final native String getTextFrom() /*-{
-		return this.textFrom;
-	}-*/;
-
-	public final native String getTranslation() /*-{
+	public final native TranslationResult getTranslation() /*-{
 		return this.translation;
 	}-*/;
 
-	public static final native PrepareTranslatedTextDisplayMessage create(
-			String textFrom, String translation, String srcLang, String destLang)/*-{
-		return {textFrom: textFrom, translation: translation, src: srcLang, dest: destLang, type:'PrepareTranslatedTextDisplayMessage'};
+	public static final native TranslationResultMessage create(TranslationResult translationResult)/*-{
+		return {translation: translationResult, type:'TranslationResultMessage'};
 	}-*/;
 
 }
