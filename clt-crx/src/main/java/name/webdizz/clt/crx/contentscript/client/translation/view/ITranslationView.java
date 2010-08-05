80c27247541f19a0e9743055fe827286027e7bc5
webdizz <webdizz@gmail.com>
2010-08-05 11:46:11 +0300

    TranslationView was refactored

--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/translation/view/ITranslationView.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/translation/view/ITranslationView.java
@@ -1,9 +1,30 @@
 package name.webdizz.clt.crx.contentscript.client.translation.view;
 
+import com.google.gwt.user.client.ui.Widget;
+
 public interface ITranslationView {
 
-	public abstract void setTranslatedText(final String text);
+	/**
+	 * Sets translated word.
+	 * 
+	 * @param text
+	 *            the translation result
+	 */
+	void setTranslatedText(String text);
 
-	public abstract void setTranslateableText(final String text);
+	/**
+	 * Sets source word.
+	 * 
+	 * @param text
+	 *            the source word to translate
+	 */
+	void setTranslateableText(String text);
+
+	/**
+	 * Convert {@link ITranslationView} to {@link Widget}.
+	 * 
+	 * @return {@link ITranslationView} as a {@link Widget}
+	 */
+	Widget asWidget();
 
 }
--- clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/translation/view/TranslationView.java
+++ clt-crx/src/main/java/name/webdizz/clt/crx/contentscript/client/translation/view/TranslationView.java
@@ -4,7 +4,6 @@
 package name.webdizz.clt.crx.contentscript.client.translation.view;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JsonUtils;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -17,45 +16,53 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class TranslationView extends Composite implements ITranslationView {
 
-	private static TranslationUiBinder uiBinder = GWT
-			.create(TranslationUiBinder.class);
+	private static TranslationUiBinder uiBinder = GWT.create(TranslationUiBinder.class);
 
 	interface TranslationUiBinder extends UiBinder<Widget, TranslationView> {
 	}
 
 	@UiField
 	SpanElement translatedText;
-	
+
 	@UiField
-	public
-	SpanElement translation;
+	public SpanElement translation;
 
 	public TranslationView() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
-	/* (non-Javadoc)
-	 * @see name.webdizz.clt.crx.contentscript.client.translation.view.ITranslationView#setTranslatedText(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * name.webdizz.clt.crx.contentscript.client.translation.view.ITranslationView
+	 * #setTranslatedText(java.lang.String)
 	 */
-	@Override
 	public void setTranslatedText(final String text) {
 		translatedText.setInnerText(text);
 		translation.setAttribute("translation", text);
 	}
-	
-	/* (non-Javadoc)
-	 * @see name.webdizz.clt.crx.contentscript.client.translation.view.ITranslationView#setTranslateableText(java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * name.webdizz.clt.crx.contentscript.client.translation.view.ITranslationView
+	 * #setTranslateableText(java.lang.String)
 	 */
-	@Override
 	public void setTranslateableText(final String text) {
 		translation.setAttribute("translateable", text);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * name.webdizz.clt.crx.contentscript.client.translation.view.ITranslationView
+	 * #asWidget()
+	 */
 	public Widget asWidget() {
 		return this;
 	}
 
-	public String widgetAsString() {
-		return JsonUtils.escapeValue(toString());
-	}
 }
